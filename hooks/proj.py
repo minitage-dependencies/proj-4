f7a9b3f703eeb82a4d4a50be5be844d32ec5105e
Simon Thépot <simon.thepot@gmail.com>
2010-11-15 15:50:58 +0100

    up

--- hooks/proj.py
+++ hooks/proj.py
@@ -53,4 +53,14 @@ def h(options,buildout):
         os.system("autoreconf -ifv")
     installmaps(options, buildout)
 
+def p(options, buildout):
+    ip = os.path.join(options['location'], 'share', 'proj', 'epsg')
+    fp = open(ip, 'a')
+    fp.write("""
+# Google Spherical Mercator projection
+<900913> +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over<> 
+""")
+    fp.flush()
+    fp.close()
+
 
